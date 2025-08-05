72ab8496130a17011d9c1b1469b2fe72c1195d54
Subho44 <98902921+Subho44@users.noreply.github.com>
2025-08-05 22:26:56 +0530

    done home
    card,slider,navbar,routing

--- E-learning-web/client/src/components/Coursecard.jsx
+++ E-learning-web/client/src/components/Coursecard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Card,Button } from 'react-bootstrap'
 
-const Coursecard = () => {
-  return (
-    <div>Coursecard</div>
-  )
+const Coursecard = ({course}) => {
+  return <>
+    <Card data-aos="fade-up" style={{width:'18rem', margin:'1rem'}}>
+    <Card.Img variant='top' src={course.image}/>
+   <Card.Body>
+    <Card.Title>{course.title}</Card.Title>
+    <Card.Text>{course.description}</Card.Text>
+    <Button variant='primary'>Enroll Now</Button>
+   </Card.Body>
+
+    </Card>
+  </>
 }
 
 export default Coursecard
